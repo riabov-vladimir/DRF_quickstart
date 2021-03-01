6757afa99be22f4c4eeec7054e57acd72287565a
Wolodia <Riabow>
2021-03-01 23:04:49 +0300

    urls.py

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from tutorial.quickstart.views import UserViewSet, GroupViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
